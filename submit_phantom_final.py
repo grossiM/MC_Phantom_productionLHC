8f747ce348c7ebbbe472ac4a2e2644db4cfa4e31
Michele Grossi <33418276+grossiM@users.noreply.github.com>
2020-04-24 12:32:20 +0200

    add files

--- submit_phantom_final.py
+++ submit_phantom_final.py
@@ -638,7 +638,44 @@ def gridpackGeneration (debugging):
     # launch the submission script
     execute ('source ' + submitfilename, debugging)
 
-    
+    # wait until all jobs finish, before calculating the cross-section
+    # and compressing the gridpack
+    # finished = False
+    # while not finished:
+    #     finished = True
+    #     unfinished = int (0)
+    #     for fil in processoutputs:
+    #         if not os.path.exists (fil):
+    #             finished = False
+    #             unfinished += 1
+    #     sys.stdout.write ('waiting for: ' + str (unfinished) + ' jobs\r' )
+    #     sys.stdout.flush ()
+    #     time.sleep (60) # seconds
+
+    #log file of the generation parameters
+    # logfilename = workingfolder + '/log_GRID.txt'
+    # logfile = open (logfilename, 'write')
+
+    # # calculate the cross-section
+    # command = 'cd ' + workingfolder + '; grep SIGMA */run.out > res ; '
+    # command += workingfolder + '/' + phantomfolder + '/tools/totint.exe > result '
+    # execute (command, debugging)
+    # result = execute ('tail -n 1 ' + workingfolder + '/result', debugging)
+    # Xsection = result[1] + ' pb'
+
+    # verifyGridpack (processoutputs, workingfolder, logfile)
+
+    # logfile.write ('CONFIG FILE\n\n')
+    # for section in config.sections ():
+    #     options = config.options (section)
+    #     for option in options:
+    #         logfile.write ('[' + section + '] ' + option + ' : ' + config.get (section, option) + '\n')
+    # logfile.write ('\nSETUP COMMAND\n\n')
+    # logfile.write (command + '\n')
+    # logfile.write ('\nRESULTING CROSS-SECTION FROM GRID CREATION\n\n')
+    # logfile.write (Xsection + '\n')
+    # logfile.close ()
+
     # NB removing the phantom to save space, it will have to be put back (reasonably in the same place)
     #    when generating the events
     if not debugging:
@@ -646,7 +683,23 @@ def gridpackGeneration (debugging):
         execute ('rm -rf CMSSW*', debugging)
         execute ('rm -rf LSFJOB*', debugging)
 
-    
+    # get all the grids, to be run in workingfolder
+#     gridfiles = execute ('for fil in `find -L . -name "phavegas*.dat"` ; do echo $fil ; done', debugging)
+
+#     # prepare the r.in file for the event production, starting from the template one
+#     replacement = {'ionesh':'1\n', 'nfiles': str(len (gridfiles[1].split ()))+'\n', 'nunwevts':'EVENTSNUM\n', 'idum':'-RANDOMSEED\n'}
+#     replaceParameterInFile (workingfolder + '/r.in', workingfolder + '/r_GEN.in', replacement)
+#     addGridsToRin (workingfolder + '/r_GEN.in', gridfiles[1], debugging)
+
+#     execute ('mv r.in r_GRID.in', debugging)
+
+#     # prepare the script for the event generation
+
+#     execute ('cp ../' + sys.argv[1] + ' ./', debugging)
+
+# #this must be replaced
+#     execute ('tar cJf ../' + foldername.split ('/')[-1] + '.tar.xz *', debugging)
+#     print 'gridpack ' + foldername.split ('/')[-1] + '.tar.xz created'
 
     os.chdir (rootfolder)
     sys.exit (0) #check questa chiusura
